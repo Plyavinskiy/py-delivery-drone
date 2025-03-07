c47ae11e71a8cb18a599fd2a60a8dd06ebc93324
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2025-03-07 12:47:43 +0200

    make max_load_weight a required parameter in DeliveryDrone

--- app/main.py
+++ app/main.py
@@ -77,8 +77,8 @@ class DeliveryDrone(FlyingRobot):
         self,
         name: str,
         weight: int,
+        max_load_weight: int,
         coords: list[int] | None = None,
-        max_load_weight: int = 0,
         current_load: Cargo | None = None
     ) -> None:
         super().__init__(name, weight, coords)
